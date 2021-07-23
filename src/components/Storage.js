afffbbb0ff79f5c5a54df7ab4768176a9b1d8682
Joshuafrankle <joshuafrankle7@gmail.com>
2021-07-23 11:16:20 +0530

    Testing

--- src/components/Storage.js
+++ src/components/Storage.js
@@ -1,4 +1,4 @@
-const endpoint = "https://pattarai-grievances.herokuapp.com"; // http://localhost:5000
+const endpoint = "http://localhost:5000"; //  https://pattarai-grievances.herokuapp.com
 
 module.exports = {
   endpoint,
