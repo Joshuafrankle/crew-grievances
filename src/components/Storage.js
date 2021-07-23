01e90648d88882463a507cd8901b36b2cf63334c
Joshuafrankle <joshuafrankle7@gmail.com>
2021-07-23 09:36:58 +0530

    Modifing routes

--- src/components/Storage.js
+++ src/components/Storage.js
@@ -1,9 +1,5 @@
-const userToken = "cvxuvrrhvmoixjderqgtrplidyatatmk";
-const adminToken = "ltbfuuolbhuvaxgbsgzpdnksnfefedpr";
 const endpoint = "https://pattarai-grievances.herokuapp.com"; // http://localhost:5000
 
 module.exports = {
-  userToken,
-  adminToken,
   endpoint,
 };
--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -1,14 +1,36 @@
 import { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
+import Problem from "../components/Problem";
 import Loader from "../components/Loader";
-import { userToken, adminToken } from "../components/Storage";
 
 export default function HomeRoute(props) {
   const Component = props.component;
+  const [isLoading, setIsLoading] = useState(false);
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState("");
 
   useEffect(() => {
+    fetch(`${endpoint}/api/submit`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(submitData),
+    }).then((res) => {
+      res
+        .json()
+        .then((data) => {
+          if (data.status === "success") {
+            history.push("/thankyou");
+          } else if (data.status === "failure") {
+            //console.log(data.reason);
+            setError(true);
+          }
+        })
+        .catch(() => {
+          setError(true);
+        });
+    });
     const token = window.localStorage.getItem("token");
     if (token === userToken) {
       setUser("user");
