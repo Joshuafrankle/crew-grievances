d4f0187d0ab368d195e2c42376222f89f8a28afe
Joshuafrankle <joshuafrankle7@gmail.com>
2021-07-23 10:21:38 +0530

    Changes in admin route

--- src/routes/AdminRoute.js
+++ src/routes/AdminRoute.js
@@ -1,25 +1,44 @@
 import { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
+import Problem from "../components/Problem";
 import Loader from "../components/Loader";
-import { adminToken } from "../components/Storage";
 
 export default function HomeRoute(props) {
   const Component = props.component;
+  const token = window.localStorage.getItem("token");
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
-    const token = window.localStorage.getItem("token");
-    if (token === adminToken) {
-      setIsAdmin(true);
-    }
-    setLoading(false);
+    fetch(`${endpoint}/api/check`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(token),
+    }).then((res) => {
+      res
+        .json()
+        .then((data) => {
+          if (data.user === "admin") {
+            setIsAdmin(true);
+          }
+          setLoading(false);
+        })
+        .catch(() => {
+          setError(true);
+          setLoading(false);
+        });
+    });
   }, []);
 
   return (
     <>
       {loading ? (
         <Loader />
+      ) : error ? (
+        <Problem />
       ) : isAdmin ? (
         <Component />
       ) : (
