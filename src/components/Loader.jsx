cfc2446af75a258977d3a3aad5958f54b869f74e
Joshua Frankle <joshuafrankle7@gmail.com>
2021-07-23 16:50:33 +0530

    Diff width

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,32 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import LinearProgress from "@material-ui/core/LinearProgress";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-
-function LinearProgressWithLabel(props) {
-  return (
-    <Box display="flex" alignItems="center">
-      <Box width="100%" mr={1}>
-        <LinearProgress variant="determinate" {...props} />
-      </Box>
-      <Box minWidth={35}>
-        <Typography variant="body2" color="textSecondary">{`${Math.round(
-          props.value
-        )}%`}</Typography>
-      </Box>
-    </Box>
-  );
-}
-
-LinearProgressWithLabel.propTypes = {
-  /**
-   * The value of the progress indicator for the determinate and buffer variants.
-   * Value between 0 and 100.
-   */
-  value: PropTypes.number.isRequired,
-};
 
 const useStyles = makeStyles({
   root: {
@@ -34,16 +8,21 @@ const useStyles = makeStyles({
   },
 });
 
-export default function LinearWithValueLabel() {
+export default function LinearDeterminate() {
   const classes = useStyles();
-  const [progress, setProgress] = React.useState(10);
+  const [progress, setProgress] = React.useState(0);
 
   React.useEffect(() => {
     const timer = setInterval(() => {
-      setProgress((prevProgress) =>
-        prevProgress >= 100 ? 10 : prevProgress + 10
-      );
-    }, 800);
+      setProgress((oldProgress) => {
+        if (oldProgress === 100) {
+          return 0;
+        }
+        const diff = Math.random() * 10;
+        return Math.min(oldProgress + diff, 100);
+      });
+    }, 500);
+
     return () => {
       clearInterval(timer);
     };
@@ -55,7 +34,7 @@ export default function LinearWithValueLabel() {
       style={{ height: "100vh", width: "100vw" }}
     >
       <div className={classes.root}>
-        <LinearProgressWithLabel value={progress} />
+        <LinearProgress variant="determinate" value={progress} />
       </div>
     </div>
   );
