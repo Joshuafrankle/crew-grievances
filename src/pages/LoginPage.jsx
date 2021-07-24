2ea9fa7765b5f2a93494a03633d3ab654e993aea
Joshuafrankle <joshuafrankle7@gmail.com>
2021-07-24 08:23:00 +0530

    Home UI

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
 
   function changePage() {
     const loginButton = document.getElementById("login_btn");
-    loginButton.setAttribute("disabled", "true");
+    loginButton.setAttribute("disabled", true);
     loginButton.innerHTML = `<div className="spinner-border spinner-border-sm" role="status"><span class="visually-hidden">Loading...</span></div>`;
 
     const invalidUser = document.getElementById("invalid_user");
@@ -42,8 +42,8 @@ export default function LoginPage() {
             email.setAttribute("is-invalid", true);
             password.setAttribute("is-invalid", true);
             invalidUser.classList.remove("d-none");
-            loginButton.innerHTML = "Login";
-            loginButton.removeAttribute("disabled");
+            // loginButton.innerHTML = "Login";
+            // loginButton.removeAttribute("disabled");
           }
         })
         .catch(() => {
