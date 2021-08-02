88b691a2fb2c8c6a0930eaefa415cd5686bc2e1d
Joshuafrankle <joshuafrankle7@gmail.com>
2021-08-02 08:53:35 +0530

    Changed Problem message

--- src/components/Problem.jsx
+++ src/components/Problem.jsx
@@ -7,7 +7,7 @@ export default function Problem() {
         <div>
           <div className="glass text-center" style={{ marginTop: "200px" }}>
             <img className="mb-4" src={Tick} alt="Done" />
-            <h3 className="text-white">Error</h3>
+            <h3 className="text-white">Uh Oh!</h3>
             <h4 className="text-white mt-4">
               We are dealing with an Internal error. Try again sometime later
             </h4>
