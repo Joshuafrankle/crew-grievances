c990a66fe06e9145ba5a0b09a24ae8c901ae198c
Joshuafrankle <joshuafrankle7@gmail.com>
2021-08-09 09:08:37 +0530

    UI changes

--- src/pages/grievances/GrievanceList.jsx
+++ src/pages/grievances/GrievanceList.jsx
@@ -1,4 +1,4 @@
-import Logo from "../../assets/images/logo.png";
+// import Logo from "../../assets/images/logo.png";
 import FadeIn from "../../components/FadeIn";
 import { useHistory } from "react-router-dom";
 import Problem from "../../components/Problem";
@@ -41,58 +41,44 @@ export default function GrievanceList({ theData }) {
         <Problem />
       ) : (
         <FadeIn>
-          <div className="text-end logout-btn">
-            <button
-              id="grievance_logout_btn"
-              type="button"
-              className="btn mt-4 home-logout"
-              style={{ position: "absolute" }}
-              onClick={handleLogout}
-            >
-              Logout
-            </button>
-          </div>
-          <section className="main">
-            <div className="" style={{ height: "100vh", width: "80%" }}>
-              <div
-                className="d-flex"
-                style={{
-                  paddingTop: "50px",
-                  width: "100%",
-                }}
-              >
-                <div className="" style={{ marginRight: "auto" }}>
-                  <p
-                    className="pattarai-text home-pattarai-text  mb-0"
-                    style={{ marginLeft: "8px" }}
-                  >
-                    PATTARAI's
-                  </p>
-                  <p className="home-text-main">Grievance Portal</p>
-                </div>
-                <div>
-                  <img className="" src={Logo} alt="Pattarai" />
-                </div>
+          <div className="admin-main">
+            <div className="admin-head">
+              <div>
+                <p className="admin-pattarai-text mb-0">PATTARAI'S</p>
+                <p className="admin-grievance-text">Grievance Portal</p>
               </div>
-              {theData.map((grievance, id) => (
-                <FadeIn key={id}>
-                  <div className="card mb-3">
-                    <div className="card-body">
-                      <h5>
-                        <span className="badge" style={{ marginRight: "10px" }}>
-                          {grievance[1]}
-                        </span>
-                        <span className="badge">{grievance[2]}</span>
-                      </h5>
-                      <p className="card-text font-weight-bold mt-3">
-                        {grievance[3]}
-                      </p>
-                    </div>
-                  </div>
-                </FadeIn>
-              ))}
+              {/* <div>
+              <img className="admin-img" src={Logo} alt="Pattarai" />
+            </div> */}
+              <div>
+                <button
+                  id="grievance_logout_btn"
+                  type="button"
+                  className="btn admin-logout-btn"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+              </div>
             </div>
-          </section>
+            {theData.map((grievance, id) => {
+              <FadeIn key={id}>
+                <div className="card mb-3">
+                  <div className="card-body">
+                    <h5>
+                      <span className="badge" style={{ marginRight: "10px" }}>
+                        {grievance[1]}
+                      </span>
+                      <span className="badge">{grievance[2]}</span>
+                    </h5>
+                    <p className="card-text font-weight-bold mt-3">
+                      {grievance[3]}
+                    </p>
+                  </div>
+                </div>
+              </FadeIn>;
+            })}
+          </div>
         </FadeIn>
       )}
     </>
