55df6b9930a41336c76670ab5bb1ffe2988bf775
Joshuafrankle <joshuafrankle7@gmail.com>
2021-08-31 10:24:10 +0530

    Implementing Custom hook useSWRPost

--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -1,57 +1,25 @@
-import { useState, useEffect } from "react";
-import { Redirect } from "react-router-dom";
 import Problem from "../components/Problem";
 import Loader from "../components/Loader";
+import { useSWRPost } from "../components/DataFetch";
 import { endpoint } from "../components/Storage";
+import { Redirect } from "react-router-dom";
 
 export default function HomeRoute(props) {
   const Component = props.component;
   const token = window.localStorage.getItem("token");
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [user, setUser] = useState("");
 
-  useEffect(() => {
-    const tokenValue = { token };
-    fetch(`${endpoint}/api/check`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(tokenValue),
-    }).then((res) => {
-      res
-        .json()
-        .then((data) => {
-          if (data.status === "failure") {
-            setError(true);
-          } else if (data.status === "false") {
-            setUser("false");
-          } else if (data.status === "expired") {
-            window.localStorage.removeItem("token");
-            setUser("false");
-          } else if (data.status === "success") {
-            setUser(`${data.role}`);
-          }
-          setTimeout(() => {
-            setLoading(false);
-          }, 3000);
-        })
-        .catch(() => {
-          setError(true);
-          setLoading(false);
-        });
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const {
+    data: user,
+    isLoading,
+    isError,
+  } = useSWRPost(`${endpoint}/api/check`, token);
+
+  if (isError) return <Problem />;
+  if (isLoading) return <Loader />;
 
   return (
     <>
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Problem />
-      ) : user === "crew" || user === "admin" ? (
+      {user.role === "crew" || user.role === "admin" ? (
         <Component />
       ) : (
         <Redirect to={{ pathname: "/" }} />
