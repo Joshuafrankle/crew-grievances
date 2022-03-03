2ae448e50b92910ed77aba913473d8831129ff0f
Joshuafrankle <joshuafrankle7@gmail.com>
2022-03-03 11:38:15 +0000

    Update endpoint

--- src/components/DataFetch.jsx
+++ src/components/DataFetch.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import useSWR from "swr";
 
+const endpoint = "https://pattarai-grievances.herokuapp.com"; // http://localhost:5000
+
 // const axiosGet = (url) => axios.get(url).then((res) => res.data);
 // const axiosPost = (url) => axios.post(url).then((res) => res.data);
 
 export function useSWRGet(url) {
-  const { data, error } = useSWR(`${url}`, (url) =>
+  const { data, error } = useSWR(`${endpoint}${url}`, (url) =>
     axios.get(url).then((res) => res.data)
   );
   return {
@@ -16,7 +18,7 @@ export function useSWRGet(url) {
 }
 
 export function useSWRPost(url, reqData) {
-  const { data, error } = useSWR(`${url}`, (url) =>
+  const { data, error } = useSWR(`${endpoint}${url}`, (url) =>
     axios.post(url, { token: reqData }).then((res) => res.data)
   );
   return {
