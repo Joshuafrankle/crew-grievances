026b792860a7177bc85569500f847ff767f10d31
Joshuafrankle <joshuafrankle7@gmail.com>
2022-03-05 12:52:17 +0000

    default params

--- src/components/DataFetch.jsx
+++ src/components/DataFetch.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const endpoint = 'http://127.0.0.1:5000/api';
 
-export function axiosRequest(url, method="GET", data={}) {
+export function axiosRequest(url="/", method="GET", data={}) {
   return axios.request({
     method: method,
     url: `${endpoint}${url}`,
