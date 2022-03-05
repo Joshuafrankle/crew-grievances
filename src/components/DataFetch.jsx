adc03cc771b8932d3085b63d5963f1ca021ff098
Joshuafrankle <joshuafrankle7@gmail.com>
2022-03-05 12:51:15 +0000

    Update data fetch

--- src/components/DataFetch.jsx
+++ src/components/DataFetch.jsx
@@ -2,32 +2,9 @@ import axios from 'axios';
 
 const endpoint = 'http://127.0.0.1:5000/api';
 
-export function axiosGet(url) {
+export function axiosRequest(url, method="GET", data={}) {
   return axios.request({
-    method: 'GET',
-    url: `${endpoint}${url}`,
-  });
-}
-
-export function axiosPost(url, data={}) {
-  return axios.request({
-    method: 'POST',
-    url: `${endpoint}${url}`,
-    data: data,
-  });
-}
-
-export function axiosPatch(url, data={}) {
-  return axios.request({
-    method: 'PATCH',
-    url: `${endpoint}${url}`,
-    data: data,
-  });
-}
-
-export function axiosDelete(url, data={}) {
-  return axios.request({
-    method: 'DELETE',
+    method: method,
     url: `${endpoint}${url}`,
     data: data,
   });
