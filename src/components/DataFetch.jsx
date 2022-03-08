afe797c7d17645c3617454b0ba9896b87dbcf9a0
Joshuafrankle <joshuafrankle7@gmail.com>
2022-03-08 12:33:15 +0000

    made async

--- src/components/DataFetch.jsx
+++ src/components/DataFetch.jsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const endpoint = 'http://127.0.0.1:5000/api';
 
-export function axiosRequest(url="/", method="GET", data={}) {
-  return axios.request({
+export async function axiosRequest(url="/", method="GET", data={}) {
+  return await axios.request({
     method: method,
     url: `${endpoint}${url}`,
     data: data,
