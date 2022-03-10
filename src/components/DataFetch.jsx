658984c71869b01ca5f0ebbc4507cc95f42b9a25
Joshuafrankle <joshuafrankle7@gmail.com>
2022-03-10 08:16:54 +0000

    Update Data fetch

--- src/components/DataFetch.jsx
+++ src/components/DataFetch.jsx
@@ -2,10 +2,14 @@ import axios from 'axios';
 
 const endpoint = 'https://jsonplaceholder.typicode.com/users';
 
-export function axiosRequest(url="/", method="GET", data={}) {
+export function axiosRequest(url = "/", method = "GET", data = {}) {
+  const token = localStorage.getItem("token") ?? "null";
   return axios.request({
-    method: method,
     url: `${endpoint}${url}`,
+    method: method,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
     data: data,
   });
 }
