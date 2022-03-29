95e436423157a2e9c1806c68ce6497fe7b7fef48
Joshuafrankle <joshuafrankle7@gmail.com>
2022-03-29 05:59:33 -0700

    Update UI

--- src/pages/UserManagement.jsx
+++ src/pages/UserManagement.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function UserManagement() {
   return (
-    <div>UserManagement</div>
-  )
+    <>
+      <h1>UserManagement</h1>
+    </>
+  );
 }
