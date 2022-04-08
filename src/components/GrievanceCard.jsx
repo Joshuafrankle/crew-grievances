1f2909dfa8ffcfb6d2c68c702b7f0231d25bf41a
Joshua Frankle <joshuafrankle7@gmail.com>
2022-04-08 23:44:12 +0530

    Grievance card UI update

--- src/components/GrievanceCard.jsx
+++ src/components/GrievanceCard.jsx
@@ -1,30 +1,47 @@
 import React from "react";
 import FadeIn from "./FadeIn";
 
-export default function GrievanceCard({ grievances }) {
+export default function GrievanceCard({ grievances, setGrievanceId }) {
   return (
     <>
-      {grievances.map((grievance, index) => {
-        return (
-          <div key={`${grievance}-${index}`}>
+      <div className="row">
+        {grievances.map((grievance, index) => (
+          <div
+            className="col-lg-6 col-xl-4"
+            key={`${grievance.grievanceTitle}-${index}`}
+          >
             <FadeIn>
               <div className="card mb-3">
                 <div className="card-body">
                   <h5>
                     <span className="badge" style={{ marginRight: "10px" }}>
-                      {grievance[1]}
+                      {grievance.grievanceTitle}
                     </span>
-                    <span className="badge">{grievance[2]}</span>
+                    <span className="badge">{grievance.severity}</span>
                   </h5>
                   <p className="card-text font-weight-bold mt-3">
-                    {grievance[3]}
+                    {grievance.grievance}
                   </p>
+                  <button
+                    type="button"
+                    className="btn me-3"
+                    onClick={() => setGrievanceId(grievance.grievanceId)}
+                  >
+                    Resolve
+                  </button>
+                  <button
+                    type="button"
+                    className="btn"
+                    onClick={() => setGrievanceId(grievance.grievanceId)}
+                  >
+                    Delete
+                  </button>
                 </div>
               </div>
             </FadeIn>
           </div>
-        );
-      })}
+        ))}
+      </div>
     </>
   );
 }
