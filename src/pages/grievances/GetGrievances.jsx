1eb362afa0bc283804bec5dbbd90c1f59c53b5b4
Joshua Frankle <joshuafrankle7@gmail.com>
2022-04-08 23:03:00 +0530

    Update handling err

--- src/pages/grievances/GetGrievances.jsx
+++ src/pages/grievances/GetGrievances.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import Problem from "components/Problem";
 import Loader from "components/Loader";
 import { axiosRequest } from "components/DataFetch";
 import GrievanceList from "./DisplayGrievances";
 
 export default function GetGrievances() {
+  const history = useHistory();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [grievanceList, setGrievanceList] = useState({});
@@ -14,12 +16,19 @@ export default function GetGrievances() {
       const { data } = await axiosRequest("/admin");
       setGrievanceList(data);
     } catch ({ response }) {
-      setError(true);
+      if (response.status === 5000) {
+        setError(true);
+      } else {
+        history.push("/");
+      }
     }
     setLoading(false);
   }
 
-  useEffect(() => getGrievances(), []);
+  useEffect(() => {
+    getGrievances();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   if (loading) return <Loader />;
   else if (error) return <Problem />;
