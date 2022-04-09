68879f5109e3304ba427cc2cd1ee5b512e944f4b
Joshua Frankle <joshuafrankle7@gmail.com>
2022-04-09 11:13:15 +0530

    💄 Added Popup comp

---
+++ src/components/Popup.jsx
@@ -0,0 +1,31 @@
+import { Dialog, DialogTitle, DialogContent, IconButton } from '@mui/material';
+import { MdOutlineCancel } from 'react-icons/md';
+
+export default function Popup({ title, openModal, setOpenModal, children }) {
+  return (
+    <>
+      <Dialog
+        open={openModal}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          <div className="d-flex justify-content-between align-items-center">
+            <h2>{title}</h2>
+            <IconButton
+              color="error"
+              aria-label="close"
+              component="span"
+              onClick={() => setOpenModal(false)}
+            >
+              <MdOutlineCancel />
+            </IconButton>
+          </div>
+        </DialogTitle>
+        <DialogContent>
+          <div className="mt-2">{children}</div>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
