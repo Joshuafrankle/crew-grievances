70fc0838a04111a0433f9902f7903584fa1b2e00
Joshua Frankle <joshuafrankle7@gmail.com>
2022-04-12 13:41:55 +0530

    ♻️ Combined GetGrievance and DisplayGrievance

--- src/pages/grievances/DisplayGrievances.jsx
+++ src/pages/grievances/DisplayGrievances.jsx
@@ -1,36 +1,102 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import { axiosRequest } from "components/DataFetch";
+import Problem from "components/Problem";
+import Loader from "components/Loader";
 import FadeIn from "components/FadeIn";
-import GetGrievances from "./GetGrievances";
+import Popup from "components/Popup";
+import GrievanceCard from "./GrievanceCard";
+import ResolveForm from "./ResolveForm";
+import DeleteForm from "./DeleteForm";
 
 export default function DisplayGrievances() {
   const history = useHistory();
+  const [openPopup, setOpenPopup] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [grievanceList, setGrievanceList] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axiosRequest("/admin");
+        setGrievanceList(data.grievanceList);
+      } catch ({ response }) {
+        if (response.status === 5000) {
+          setError(true);
+        } else {
+          history.push("/");
+        }
+      }
+      setLoading(false);
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const [id, setId] = useState({
+    resolveId: null,
+    deleteId: null,
+  });
 
   function handleLogout() {
     localStorage.removeItem("token");
     history.push("/");
   }
 
-  return (
-    <FadeIn>
-      <div className="admin-main">
-        <div className="admin-head">
-          <div>
-            <p className="admin-pattarai-text mb-0">PATTARAI'S</p>
-            <p className="admin-grievance-text">Grievance Portal</p>
+  if (error) {
+    return <Problem />;
+  } else {
+    return (
+      <>
+        <FadeIn>
+          <div className="admin-main">
+            <div className="admin-head">
+              <div>
+                <p className="admin-pattarai-text mb-0">PATTARAI'S</p>
+                <p className="admin-grievance-text">Grievance Portal</p>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  className="btn admin-logout-btn"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+              </div>
+            </div>
+            {loading ? (
+              <Loader />
+            ) : (
+              <GrievanceCard
+                grievances={grievanceList}
+                setId={setId}
+                setOpenPopup={setOpenPopup}
+              />
+            )}
           </div>
-          <div>
-            <button
-              type="button"
-              className="btn admin-logout-btn"
-              onClick={handleLogout}
-            >
-              Logout
-            </button>
-          </div>
-        </div>
-        <GetGrievances />
-      </div>
-    </FadeIn>
-  );
+        </FadeIn>
+
+        <Popup
+          title={id.deleteId ? "Are you sure wanna delete?" : "Member Form"}
+          openModal={openPopup}
+          setOpenModal={setOpenPopup}
+        >
+          {id.deleteId ? (
+            <DeleteForm
+              id={id.deleteId}
+              setError={setError}
+              setOpenModal={setOpenPopup}
+            />
+          ) : (
+            <ResolveForm
+              id={id.resolveId}
+              setError={setError}
+              setOpenModal={setOpenPopup}
+            />
+          )}
+        </Popup>
+      </>
+    );
+  }
 }
