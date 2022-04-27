39328baf87c06ee5a5b3264b14a689da54d0b64a
Joshuafrankle <joshuafrankle7@gmail.com>
2022-04-27 03:41:38 -0700

    removed default url

--- src/components/DataFetch.jsx
+++ src/components/DataFetch.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const endpoint = "http://localhost:5000/api";
 
-export function axiosRequest(url = "/", method = "GET", data = {}) {
+export function axiosRequest(url, method = "GET", data = {}) {
   const token = localStorage.getItem("token") ?? "null";
   return axios.request({
     url: `${endpoint}${url}`,
