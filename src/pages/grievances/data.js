e84702e14a771d0130485a59e1c3f6e491d2350f
Joshuafrankle <joshuafrankle7@gmail.com>
2022-05-07 01:33:30 -0700

    Sample data

---
+++ src/pages/grievances/data.js
@@ -0,0 +1,17 @@
+const data = [
+  {
+    grievanceId: "id1",
+    grievanceTitle: "College",
+    grievance:
+      " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore expedita, natus qui error minus exercitationem! Obcaecati quasi explicabo consequuntur modi deserunt nemo? Obcaecati, beatae fugiat fugit maiores fuga veniam alias.",
+    severity: "Enhancement",
+  },
+  {
+    grievanceId: "id2",
+    grievanceTitle: "College",
+    grievance:
+      " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore expedita, natus qui error minus exercitationem! Obcaecati quasi explicabo consequuntur modi deserunt nemo? Obcaecati, beatae fugiat fugit maiores fuga veniam alias.",
+    severity: "Compilant",
+  },
+];
+export { data };
