051001c3ce6973898847be85321d491302948c48
Joshua Frankle <joshuafrankle7@gmail.com>
2022-06-11 01:32:32 +0530

    Added browser route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import PrivateRoute from "routes/PrivateRoute";
 import LoginRoute from "routes/LoginRoute";
@@ -11,28 +11,32 @@ import Error404 from "pages/Error404";
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<LoginRoute />}>
-        <Route path="/" element={<LoginPage />} />
-      </Route>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<LoginRoute />}>
+          <Route path="/" element={<LoginPage />} />
+        </Route>
 
-      <Route
-        element={
-          <PrivateRoute allowedRoles={["user", "admin", "superAdmin"]} />
-        }
-      >
-        <Route path="/home" element={<Home />} />
-      </Route>
+        <Route
+          element={
+            <PrivateRoute allowedRoles={["user", "admin", "superAdmin"]} />
+          }
+        >
+          <Route path="/home" element={<Home />} />
+        </Route>
 
-      <Route element={<PrivateRoute allowedRoles={["admin", "superAdmin"]} />}>
-        <Route path="/grievance-list" element={<DisplayGrievances />} />
-      </Route>
+        <Route
+          element={<PrivateRoute allowedRoles={["admin", "superAdmin"]} />}
+        >
+          <Route path="/grievance-list" element={<DisplayGrievances />} />
+        </Route>
 
-      <Route element={<PrivateRoute allowedRoles={["superAdmin"]} />}>
-        <Route path="/user-manage" element={<UserManagement />} />
-      </Route>
+        <Route element={<PrivateRoute allowedRoles={["superAdmin"]} />}>
+          <Route path="/user-manage" element={<UserManagement />} />
+        </Route>
 
-      <Route path="*" element={<Error404 />} />
-    </Routes>
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
