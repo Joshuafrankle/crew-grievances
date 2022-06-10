1c481f38455148e342166472d5d3a0bf11e49866
Joshua Frankle <joshuafrankle7@gmail.com>
2022-06-11 01:32:15 +0530

    Added dataFetch comp

---
+++ src/components/DataFetch.ts
@@ -0,0 +1,34 @@
+import React from "react";
+import axios, { Method } from "axios";
+
+export default async function DataFetch(
+  endpoint: string,
+  method: Method = "GET",
+  axiosData = {}
+) {
+  const token = localStorage.getItem("token") ?? "null";
+  try {
+    const res = await axios.request({
+      url: `${process.env.REACT_APP_BACKEND_URL}${endpoint}`,
+      method: method,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: axiosData,
+    });
+    console.log("Res", res);
+    return [res.data.data ? res.data.data : res.data, null];
+  } catch (err: any) {
+    let error = "";
+    if (err.name === "AbortError") {
+      return;
+    } else if (!err.response) {
+      error = "No server response";
+    } else if (err.response.status >= 500) {
+      error = "Internal server error";
+    } else {
+      error = err.message;
+    }
+    return [null, error];
+  }
+}
