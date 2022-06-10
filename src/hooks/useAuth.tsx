5497ddd1e5f0dc2c0ee0dbb99f9d47109cf54c42
Joshua Frankle <joshuafrankle7@gmail.com>
2022-06-10 14:37:18 +0530

    Created useAuth

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,25 @@
+import React, { createContext, useState, useContext } from "react";
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const [role, setRole] = useState("null");
+
+  return (
+    <AuthContext.Provider value={{ role, setRole }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuth must be used within a AuthProvider");
+  }
+
+  return context;
+};
+
+export default useAuth;
