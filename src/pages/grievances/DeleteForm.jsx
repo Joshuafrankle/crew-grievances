f25ba9902676cf4e9019812744c23ec55945a7cc
Joshua Frankle <joshuafrankle7@gmail.com>
2022-06-11 18:16:24 +0530

    added btnNo

--- src/pages/grievances/DeleteForm.jsx
+++ src/pages/grievances/DeleteForm.jsx
@@ -2,11 +2,13 @@ import React, { useRef } from "react";
 import fetchData from "components/fetchData";
 
 export default function DeleteForm({ id, setError, setOpenModal }) {
-  const btnRef = useRef(null);
+  const btnYes = useRef(null);
+  const btnNo = useRef(null);
 
   async function handleDelete() {
-    btnRef.current.disabled = true;
-    btnRef.current.innerHTML = `<div class="spinner-border p-2 spinner-border-sm" role="status" aria-hidden="true"><span class="visually-hidden">Loading...</span></div>`;
+    btnYes.current.disabled = true;
+    btnNo.current.disabled = true;
+    btnYes.current.innerHTML = `<div class="spinner-border p-2 spinner-border-sm" role="status" aria-hidden="true"><span class="visually-hidden">Loading...</span></div>`;
     try {
       await fetchData(`/admin/delete`, "PATCH", { grievanceId: id });
       setOpenModal(false);
@@ -18,7 +20,7 @@ export default function DeleteForm({ id, setError, setOpenModal }) {
   return (
     <>
       <button
-        ref={btnRef}
+        ref={btnYes}
         type="button"
         className="btn btn-outline me-3"
         onClick={handleDelete}
@@ -26,6 +28,7 @@ export default function DeleteForm({ id, setError, setOpenModal }) {
         Yes
       </button>
       <button
+        ref={btnNo}
         type="button"
         className="btn btn-outline"
         onClick={() => setOpenModal(false)}
