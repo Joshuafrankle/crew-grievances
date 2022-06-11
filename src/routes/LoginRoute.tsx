26d9e6aa9b5003e7c67bfb4d04540ee6f3ad365d
Joshua Frankle <joshuafrankle7@gmail.com>
2022-06-11 11:34:51 +0530

    Update login route

--- src/routes/LoginRoute.tsx
+++ src/routes/LoginRoute.tsx
@@ -8,16 +8,34 @@ import Problem from "components/Problem";
 export default function LoginRoute() {
   const { data, loading, error } = useFetch("/auth");
   const location = useLocation();
-  const { setRole } = useAuth();
+  const { role, setRole } = useAuth();
 
-  if (loading) {
-    return <Loader />;
-  } else if (error === "Internal server error") {
-    return <Problem />;
-  } else if (!data.role) {
-    return <Outlet />;
+  if (role === "null") {
+    if (loading) {
+      return <Loader />;
+    } else if (error === "Internal server error") {
+      return <Problem />;
+    } else if (!data.role) {
+      return <Outlet />;
+    } else {
+      setRole(data.role);
+      if (data.role === "admin") {
+        return (
+          <Navigate to="/grievance-list" state={{ from: location }} replace />
+        );
+      } else if (data.role === "superAdmin") {
+        return (
+          <Navigate to="/user-manage" state={{ from: location }} replace />
+        );
+      } else {
+        return <Navigate to="/home" state={{ from: location }} replace />;
+      }
+    }
+  } else if (role === "admin") {
+    return <Navigate to="/grievance-list" state={{ from: location }} replace />;
+  } else if (role === "superAdmin") {
+    return <Navigate to="/user-manage" state={{ from: location }} replace />;
   } else {
-    setRole(data.role);
     return <Navigate to="/home" state={{ from: location }} replace />;
   }
 }
